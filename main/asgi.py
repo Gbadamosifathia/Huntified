1afb90ba8425bef2c1fce42f626d0249833ac0b0
Gbadamosifathia <gbadamosifathia02@gmail.com>
2026-09-10 16:06:32 +0100

    order updated

--- main/asgi.py
+++ main/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from huntified.middleware import JWTAuthMiddleware
 from huntified.routing import websocket_urlpatterns
@@ -18,7 +19,7 @@ from huntified.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    "http": django_asgi_app,
     'websocket': JWTAuthMiddleware(
         URLRouter(
             websocket_urlpatterns

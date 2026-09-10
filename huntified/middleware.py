5c4577cc550da081903b15f8bc5dc723c6713d42
Gbadamosifathia <gbadamosifathia02@gmail.com>
2026-09-10 10:39:16 +0100

    chat

---
+++ huntified/middleware.py
@@ -0,0 +1,33 @@
+from urllib.parse import parse_qs
+
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import TokenError
+
+from .models import CustomUser
+
+
+@database_sync_to_async
+def get_user_from_token(token):
+    try:
+        access_token = AccessToken(token)
+        user_id = access_token['user_id']
+        return CustomUser.objects.get(id=user_id)
+    except (TokenError, CustomUser.DoesNotExist, KeyError):
+        return AnonymousUser()
+
+
+class JWTAuthMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get('query_string', b'').decode()
+        query_params = parse_qs(query_string)
+        token_list = query_params.get('token')
+
+        if token_list:
+            scope['user'] = await get_user_from_token(token_list[0])
+        else:
+            scope['user'] = AnonymousUser()
+
+        return await super().__call__(scope, receive, send)
--- main/asgi.py
+++ main/asgi.py
@@ -11,14 +11,14 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from huntified.middleware import JWTAuthMiddleware
 from huntified.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    'websocket': JWTAuthMiddleware(
         URLRouter(
             websocket_urlpatterns
         )

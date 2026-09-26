44b00f47c1abf88d57557b0358efdcbd05916d97
Gbadamosifathia <gbadamosifathia02@gmail.com>
2026-09-26 07:11:21 +0100

    websocket

--- main/settings.py
+++ main/settings.py
@@ -85,9 +85,6 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [config("REDIS_URL")],
-             "connection_kwargs": {
-                "socket_keepalive": True,
-                },
         },
     },
 }
